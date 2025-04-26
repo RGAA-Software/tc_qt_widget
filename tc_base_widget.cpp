e2ed0d985a8b3eca2cfe1220df7b2f7d6a8b42ec
RGAA-Software <hollow.ichigo.yoo@gmail.com>
2025-04-26 22:12:03 +0800

    add base widget
    Signed-off-by: RGAA-Software <hollow.ichigo.yoo@gmail.com>

---
+++ tc_base_widget.cpp
@@ -0,0 +1,47 @@
+//
+// Created by RGAA on 26/04/2025.
+//
+
+#include "tc_base_widget.h"
+
+namespace tc
+{
+
+    TcBaseWidget::TcBaseWidget(QWidget* parent) : QWidget(parent) {
+
+    }
+
+    void TcBaseWidget::enterEvent(QEnterEvent *event) {
+        QWidget::enterEvent(event);
+        if (enter_cbk_) {
+            enter_cbk_(this, true);
+        }
+    }
+
+    void TcBaseWidget::leaveEvent(QEvent *event) {
+        QWidget::leaveEvent(event);
+        if (enter_cbk_) {
+            enter_cbk_(this, false);
+        }
+    }
+
+    void TcBaseWidget::SetOnEnterWidgetCallback(OnEnterWidgetCallback&& cbk) {
+        enter_cbk_ = cbk;
+    }
+
+    void TcBaseWidget::SetOnClickWidgetCallback(OnClickWidgetCallback&& cbk) {
+        click_cbk_ = cbk;
+    }
+
+    void TcBaseWidget::mousePressEvent(QMouseEvent *event) {
+        QWidget::mousePressEvent(event);
+    }
+
+    void TcBaseWidget::mouseReleaseEvent(QMouseEvent *event) {
+        QWidget::mouseReleaseEvent(event);
+        if (click_cbk_) {
+            click_cbk_(this);
+        }
+    }
+
+}
---
+++ tc_base_widget.h
@@ -0,0 +1,41 @@
+//
+// Created by RGAA on 26/04/2025.
+//
+
+#ifndef GAMMARAY_TC_BASE_WIDGET_H
+#define GAMMARAY_TC_BASE_WIDGET_H
+
+#include <QWidget>
+#include <QPainter>
+#include <QPaintEvent>
+#include <QPen>
+#include <QBrush>
+#include <functional>
+
+namespace tc
+{
+
+    using OnEnterWidgetCallback = std::function<void(QWidget* w, bool enter)>;
+    using OnClickWidgetCallback = std::function<void(QWidget* w)>;
+
+    class TcBaseWidget : public QWidget {
+    public:
+        explicit TcBaseWidget(QWidget* parent = nullptr);
+
+        void enterEvent(QEnterEvent *event) override;
+        void leaveEvent(QEvent *event) override;
+        void mousePressEvent(QMouseEvent *event) override;
+        void mouseReleaseEvent(QMouseEvent *event) override;
+
+        void SetOnEnterWidgetCallback(OnEnterWidgetCallback&& cbk);
+        void SetOnClickWidgetCallback(OnClickWidgetCallback&& cbk);
+
+    private:
+        OnEnterWidgetCallback enter_cbk_;
+        OnClickWidgetCallback click_cbk_;
+
+    };
+
+}
+
+#endif //GAMMARAY_TC_BASE_WIDGET_H
